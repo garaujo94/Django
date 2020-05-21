d54210df2cec856388784386fc750e9e57a8230f
garaujo94 <gala.gustavoantonio@gmail.com>
2020-05-21 09:34:39 -0300

    Criação inicial da pasta templates

--- receitas/views.py
+++ receitas/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse Não é mais necessário
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Receitas</h1>')
+    return render(request,'index.html')
